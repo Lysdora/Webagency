fc26321c7b91165dd05db427d60da475923899da
Lysdora Guicherd <lysdora.web@gmail.com>
2017-10-20 06:45:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//Isotope active js code:
+//************************
+
+// Active isotope with jQuery code:
+
+$('.main-iso').isotope({
+	itemSelector: '.item',
+	layoutMode: 'fitRows'
+});
+// Isotope click function
+$('.iso-nav a').click(function(){
+	$('.iso-nav a').removeClass('active');
+	$(this).addClass('active');
+
+	var selector = $(this).attr('data-filter');
+	$('.main-iso').isotope({
+		filter: selector
+	});
+	return false;
+});
+
+
+ $(document).ready(function(){
+	 $('.carousel').flickity({
+	   // options
+	   cellAlign: 'left',
+		prevNextButtons: true,
+	   contain: true   
+                
+	 });
+     
+
+     
+
+});
